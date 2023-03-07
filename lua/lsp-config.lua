5baae25cf90577f54b27df91d25a3046a4f5174e
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-03-07 22:50:15 +0530

    Update lua/lsp-config.lua

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,52 +1,54 @@
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-	return
+    return
 end
 
 local mason_status_ok, mason = pcall(require, "mason")
 if not mason_status_ok then
-	return
+    return
 end
 
 local mason_lspconfig_status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status_ok then
-	return
+    return
 end
 
-mason.setup({})
+mason.setup({
+    ensure_installed = { "stylya", "rustfmt", "clang-format", "black", "prettier" },
+})
 
 mason_lspconfig.setup({
-	ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
+    ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "pyright", "marksman" },
 })
 
 local cnl_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cnl_ok then
-	return
+    return
 end
 
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 mason_lspconfig.setup_handlers({
-	function(server)
-		lspconfig[server].setup({
-			capabilities = capabilities,
-		})
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						-- Get the language server to recognize the `vim` global
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-	end,
-	--["rust_analyzer"] = function()
-	--	local rt_ok, rust_tools = pcall(require, "rust-tools")
-	--	if rt_ok then
-	--		rust_tools.setup({})
-	--	end
-	--end,
+    function(server)
+        lspconfig[server].setup({
+            capabilities = capabilities,
+        })
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+    end,
+    --["rust_analyzer"] = function()
+    --	local rt_ok, rust_tools = pcall(require, "rust-tools")
+    --	if rt_ok then
+    --		rust_tools.setup({})
+    --	end
+    --end,
 })
