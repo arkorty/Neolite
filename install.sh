78748db85d8bc15ef79ed98c0b97b2a1c83c1a30
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-03-08 15:25:55 +0530

    Changes to be committed:   modified:   install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-[ -d "~/.config/nvim" ] && mv ~/.config/nvim ~/.config/nvim-$(date "+%F-%T").bak
+[ -d "~/.config/nvim" ] && mv ~/.config/nvim ~/.config/nvim.bak
 
 mkdir -p ~/.config/nvim && git clone --depth 1 https://github.com/arkorty/neolite.git ~/.config/nvim
 
