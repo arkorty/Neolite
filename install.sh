c823c8b385fea602a083eb23e3fd4c49c0a829b7
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-03-08 15:47:36 +0530

    Changes to be committed:   modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-[ -d "~/.local/share/nvim/site/pack/packer/start/packer.nvim" ] && rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+[ -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ] && rm -rf $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+[ -d "$HOME/.config/nvim" ] && mv $HOME/.config/nvim $HOME/.config/nvim-(date "+%F-%T").bak
 
-[ -d "~/.config/nvim" ] && mv ~/.config/nvim ~/.config/nvim.bak
-
-mkdir -p ~/.config/nvim && git clone --depth 1 https://github.com/arkorty/neolite.git ~/.config/nvim
+mkdir -p $HOME/.config/nvim && git clone --depth 1 https://github.com/arkorty/neolite.git $HOME/.config/nvim
 
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 nvim +TSUpdate "+MasonInstall stylua rustfmt prettier black clang-format"
