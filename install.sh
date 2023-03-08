5c8d7a86dc596ae83d8d6341e44a16282f523407
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-03-08 15:50:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 [ -d "$HOME/.local/share/nvim/site/pack/packer" ] && rm -rf $HOME/.local/share/nvim/site/pack/packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-[ -d "$HOME/.config/nvim" ] && mv $HOME/.config/nvim $HOME/.config/nvim-(date "+%F-%T").bak
+[ -d "$HOME/.config/nvim" ] && mv $HOME/.config/nvim $HOME/.config/nvim-$(date "+%F-%T").bak
 
 mkdir -p $HOME/.config/nvim && git clone --depth 1 https://github.com/arkorty/neolite.git $HOME/.config/nvim
 
