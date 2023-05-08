77eb6ad3fdbe1d01241c7e17cf65de66e324e0f8
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-05-08 23:32:16 +0530

    Removed transparency plugin
    Changes to be committed:
          modified:   lua/plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,67 +1,64 @@
 local ok, packer = pcall(require, "packer")
 if not ok then
-	return
+    return
 end
 
 packer.startup(function(use)
-	-- Transparency
-	use("xiyaowong/nvim-transparent")
+    -- Base16 colorscheme
+    use("RRethy/nvim-base16")
 
-	-- Base16 colorscheme
-	use("RRethy/nvim-base16")
+    -- Nord colorscheme
+    use("shaunsingh/nord.nvim")
 
-	-- Nord colorscheme
-	use("shaunsingh/nord.nvim")
+    -- Onedark colorscheme
+    use("navarasu/onedark.nvim")
 
-	-- Onedark colorscheme
-	use("navarasu/onedark.nvim")
+    -- Gruvbox colorscheme
+    use("ellisonleao/gruvbox.nvim")
 
-	-- Gruvbox colorscheme
-	use("ellisonleao/gruvbox.nvim")
+    -- Catppuccin colorscheme
+    use({ "catppuccin/nvim", as = "catppuccin" })
 
-	-- Catppuccin colorscheme
-	use({ "catppuccin/nvim", as = "catppuccin" })
+    -- Plugin manager
+    use("wbthomason/packer.nvim")
 
-	-- Plugin manager
-	use("wbthomason/packer.nvim")
+    -- Terminal
+    use({ "akinsho/toggleterm.nvim", tag = "*" })
 
-	-- Terminal
-	use({ "akinsho/toggleterm.nvim", tag = "*" })
+    -- File tree
+    use({ "nvim-tree/nvim-tree.lua", requires = { "nvim-tree/nvim-web-devicons" }, tag = "nightly" })
 
-	-- File tree
-	use({ "nvim-tree/nvim-tree.lua", requires = { "nvim-tree/nvim-web-devicons" }, tag = "nightly" })
+    -- Statusline
+    use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
 
-	-- Statusline
-	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
+    -- Buffer tabs
+    use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
 
-	-- Buffer tabs
-	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
+    use({ "nvim-telescope/telescope.nvim", tag = "0.1.x", requires = { { "nvim-lua/plenary.nvim" } } })
 
-	use({ "nvim-telescope/telescope.nvim", tag = "0.1.x", requires = { { "nvim-lua/plenary.nvim" } } })
+    -- LSP manager
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
+    use("neovim/nvim-lspconfig")
 
-	-- LSP manager
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig")
+    -- Formatting
+    use("jose-elias-alvarez/null-ls.nvim")
 
-	-- Formatting
-	use("jose-elias-alvarez/null-ls.nvim")
+    -- Snippets
+    use("L3MON4D3/LuaSnip")          --snippet engine
+    use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
-	-- Snippets
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+    -- Completions
+    use("hrsh7th/nvim-cmp")      -- The completion plugin
+    use("hrsh7th/cmp-buffer")    -- buffer completions
+    use("hrsh7th/cmp-path")      -- path completions
+    use("saadparwaiz1/cmp_luasnip") -- snippet completions
+    use("onsails/lspkind.nvim")
+    use("hrsh7th/cmp-nvim-lsp")
 
-	-- Completions
-	use("hrsh7th/nvim-cmp") -- The completion plugin
-	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-path") -- path completions
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
-	use("onsails/lspkind.nvim")
-	use("hrsh7th/cmp-nvim-lsp")
+    -- Syntax highlighting
+    use("nvim-treesitter/nvim-treesitter")
 
-	-- Syntax highlighting
-	use("nvim-treesitter/nvim-treesitter")
-
-	-- Rust tools
-	--use("simrat39/rust-tools.nvim")
+    -- Rust tools
+    --use("simrat39/rust-tools.nvim")
 end)
