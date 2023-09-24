1a1cd80eea7be98b91fe894bfb3523fd6b8e7ccd
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-09-25 00:26:24 +0530

    Format code

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,65 +1,65 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
 		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	'lukas-reineke/indent-blankline.nvim',
-	'lewis6991/gitsigns.nvim',
-	'xiyaowong/transparent.nvim',
-	'shaunsingh/nord.nvim',
-	'Mofiqul/dracula.nvim',
-	'navarasu/onedark.nvim',
-	'ellisonleao/gruvbox.nvim',
-	'sainnhe/everforest',
-	'williamboman/mason.nvim',
-	'williamboman/mason-lspconfig.nvim',
-	'neovim/nvim-lspconfig',
-	'jose-elias-alvarez/null-ls.nvim',
-	'L3MON4D3/LuaSnip',
-	'rafamadriz/friendly-snippets',
-	'hrsh7th/nvim-cmp',
-	'hrsh7th/cmp-buffer',
-	'hrsh7th/cmp-path',
-	'saadparwaiz1/cmp_luasnip',
-	'onsails/lspkind.nvim',
-	'hrsh7th/cmp-nvim-lsp',
-	'nvim-treesitter/nvim-treesitter',
-	'simrat39/rust-tools.nvim',
-	{ 'catppuccin/nvim', as = 'catppuccin' },
-	{ 'akinsho/toggleterm.nvim', version = '*', config = true },
-	{ 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons', opt = true } },
-	{ 'nvim-tree/nvim-tree.lua', dependencies = { 'nvim-tree/nvim-web-devicons' }, tag = 'nightly' },
+	"lukas-reineke/indent-blankline.nvim",
+	"lewis6991/gitsigns.nvim",
+	"xiyaowong/transparent.nvim",
+	"shaunsingh/nord.nvim",
+	"Mofiqul/dracula.nvim",
+	"navarasu/onedark.nvim",
+	"ellisonleao/gruvbox.nvim",
+	"sainnhe/everforest",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"jose-elias-alvarez/null-ls.nvim",
+	"L3MON4D3/LuaSnip",
+	"rafamadriz/friendly-snippets",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"saadparwaiz1/cmp_luasnip",
+	"onsails/lspkind.nvim",
+	"hrsh7th/cmp-nvim-lsp",
+	"nvim-treesitter/nvim-treesitter",
+	"simrat39/rust-tools.nvim",
+	{ "catppuccin/nvim", as = "catppuccin" },
+	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	{ "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons", opt = true } },
+	{ "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" }, tag = "nightly" },
 	{
-		'akinsho/bufferline.nvim',
-		version = '*',
-		dependencies = 'nvim-tree/nvim-web-devicons',
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
 	},
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.2',
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
 		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
-		'nvim-neo-tree/neo-tree.nvim',
-		branch = 'v3.x',
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
 		dependencies = {
-			'nvim-lua/plenary.nvim',
-			'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-			'MunifTanjim/nui.nvim',
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
 		},
 	},
 }
 
 local opts = {}
 
-require('lazy').setup(plugins, opts)
+require("lazy").setup(plugins, opts)
--- lua/setup/null-ls.lua
+++ lua/setup/null-ls.lua
@@ -3,7 +3,8 @@ if not ok then
 	return
 end
 
---local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.rustfmt,
@@ -12,22 +13,23 @@ null_ls.setup({
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.clang_format,
 	},
+
 	-- you can reuse a shared lspconfig on_attach callback here
-	--on_attach = function(client, bufnr)
-	--	if client.supports_method("textDocument/formatting") then
-	--		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-	--		vim.api.nvim_create_autocmd("BufWritePre", {
-	--			group = augroup,
-	--			buffer = bufnr,
-	--			callback = function()
-	--				vim.lsp.buf.format({
-	--					bufnr = bufnr,
-	--					filter = function()
-	--						return client.name == "null-ls"
-	--					end,
-	--				})
-	--			end,
-	--		})
-	--	end
-	--end,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						bufnr = bufnr,
+						filter = function()
+							return client.name == "null-ls"
+						end,
+					})
+				end,
+			})
+		end
+	end,
 })
