66be18796e95b116ae6f83aa88b083ee65972408
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-04-11 21:02:00 +0530

    Add Neo-tree back

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,6 +20,16 @@ local plugins = {
             -- refer to the configuration section below
         }
     },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
     { "stevearc/dressing.nvim",   event = "VeryLazy" },
     { "ellisonleao/gruvbox.nvim", priority = 1000,   config = true },
     {
