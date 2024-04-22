23eccedd7ba55d714a9ef4aa2b2c9f17f99ec1a3
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-04-22 13:01:03 +0530

    Chore: update colorscheme

--- lua/setup/colorscheme.lua
+++ lua/setup/colorscheme.lua
@@ -1,8 +1,8 @@
---local ok, _colors = pcall(require, "melange")
---if not ok then
---    return
---end
+local ok, _ = pcall(require, "onedark")
+if not ok then
+    return
+end
 
 -- setup must be called before loading
 vim.opt.termguicolors = true
-vim.cmd.colorscheme("melange")
+vim.cmd.colorscheme("onedark")
