ff621ae0a2171984a0ebd8c31024e6019cb33d00
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-05-25 03:50:35 +0530

    Fix bug introduced due to change of vim syntax option type from boolean to string

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,6 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.relativenumber = true
 vim.o.smartindent = true
-vim.o.syntax = true
+vim.o.syntax = "true"
 vim.o.cursorline = true
 vim.o.signcolumn = "auto"
